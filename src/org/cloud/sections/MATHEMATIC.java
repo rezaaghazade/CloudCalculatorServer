a60d2f3cd56c84422d340c4f66c10aee736511b0
reza <reza.zah1991@gmail.com>
2014-08-04 20:40:02 +0430

    Initial commit

---
+++ src/org/cloud/sections/MATHEMATIC.java
@@ -0,0 +1,18 @@
+package org.cloud.sections;
+
+/**
+ * Created by reza on 7/23/14.
+ */
+public class MATHEMATIC {
+
+    public Double Sum(Double a,Double b)
+    {
+        System.out.println("in sum");
+        return a+b;
+    }
+    public Double Minus(Double a,Double b)
+    {
+        System.out.println("in minus");
+        return a-b;
+    }
+}
